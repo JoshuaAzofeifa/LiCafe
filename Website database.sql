2eb6ef4a720951b3063f661a08b8adea6f8a6a98
LiCafe <95188717+JoshuaAzofeifa@users.noreply.github.com>
2026-07-15 16:15:54 -0600

    Refactor database schema with unique constraints
    Updated users table to enforce unique constraints on username, email, and password_hash. Increased summary length in literature table.

--- Website database.sql
+++ Website database.sql
@@ -3,24 +3,17 @@ USE my_databaseplatform;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL, 
-    email VARCHAR(50) NOT NULL, 
-    password_hash VARCHAR(255) NOT NULL,
-    
-    role ENUM('writer', 'reader') DEFAULT 'reader',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    username VARCHAR(50) NOT NULL UNIQUE, 
+    email VARCHAR(50) NOT NULL UNIQUE, 
+    password_hash VARCHAR(255) NOT NULL UNIQUE,
 );
---INSERT INTO users (id, username, email, password_hash) VALUES 
-  --(1, 'john_doe', 'john.doe@example.com', 'hashed_password_1'),
-  --(2, 'jane_smith', 'jane.smith@example.com', 'hashed_password_2');
 
 CREATE TABLE IF NOT EXISTS literature (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(60) NOT NULL,
     writer_id INT,
     Genre ENUM('Poetry', 'Drama', 'Novel', 'Short story', 'Essay', 'Fiction', 'Theatre') NOT NULL,
-    publication_date DATE,
-    Summary VARCHAR(255) NOT NULL, 
+    Summary VARCHAR(400) NOT NULL, 
     pdf_url VARCHAR(512) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     FOREIGN KEY (writer_id) REFERENCES users(id) ON DELETE CASCADE
