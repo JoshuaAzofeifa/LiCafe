f6ff28f945618c2f5d78e0141f1f0c813a76d801
LiCafe <95188717+JoshuaAzofeifa@users.noreply.github.com>
2026-07-18 14:12:29 -0600

    Serve static files from the uploads directory

--- Database.js
+++ Database.js
@@ -16,6 +16,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(express.static(__dirname))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use(cors({
     origin: [
